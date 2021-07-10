9db93085aa54e6cb5d5b76857ace2024db7414a6
jackchen32 <86872075+Weiting71924@users.noreply.github.com>
2021-07-10 07:33:28 +0000

    Initial commit

---
+++ 20210703/0703.py
@@ -0,0 +1,15 @@
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print(pi)
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print(pi)
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print('asg'*5)
---
+++ HW2/hw.py
@@ -0,0 +1,3 @@
+b = int(input("Please input Celsius Temperature:"))
+answer = str((b - 32) * 5 / 9)
+print("\n=" + answer + "℃")
