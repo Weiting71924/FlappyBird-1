0365729fd16d1583e041c0e1ec83beda40ba73cb
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-25 10:16:07 +0000

    25

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
-import turtle as t
-t.hideturtle()
-t.pensize(3)
-t.color("red")
-t.penup()
-t.goto(0,60)
-t.pendown()
-t.circle(50)
-t.right(90)
-t.forward(180)
-t.right(45)
-t.forward(100)
-t.backward(100)
-t.left(90)
-t.forward(100)
-t.backward(100)
-t.goto(0,0)
-t.forward(100)
-t.backward(100)
-t.right(90)
-t.forward(100)
-t.backward(100)
-t.penup()
-t.goto(-20,115)
-t.shape("circle")
-t.turtlesize(0.5)
-t.stamp()
-t.goto(20,115)
-t.stamp()
-t.goto(-20,95)
-t.pendown()
-t.goto(0,83)
-t.goto(20,95)
-t.penup()
-t.home()
-t.right(90)
-i=290
-while i>-290:
-  i-=50
-  t.goto(-75,i)
-  t.pendown()
-  t.pencolor("red")
-  t.forward(30)
-  t.pencolor("white")
-  t.backward(30)
-  t.speed(0)
+l_pork=['豬肉']
+l_lobo=['胡蘿蔔']
+l_bech=['荸薺']
+while True:
+  print('\n選擇步驟')
+  print("\n1  增加菜單")
+  print("\n2  顯示菜單")
+  q=input('\n輸入步驟:')
+  if q=='1':
+    print('\n選擇材料')
+    print('\na  胡蘿蔔')
+    print('\nb  荸薺')
+    print('\nc  豬肉')
+    t=input('\n輸入代號:')
+    if t=='a':
+      l=input('\n輸入克數:')
+      l_lobo.append(l+'g')
+      if l in l_lobo:
+        continue
+      ll=input('\n刀工:')
+      l_lobo.append(ll)
+    if t=='b':
+      b=input('\n輸入克數:')
+      l_lobo.append(b+'g')
+      bb=input('刀工:')
+      l_lobo.append(bb)
+    if t=='c':
+      p=input('\n輸入克數:')
+      l_lobo.append(p+'g')
+      pp=input('刀工:')
+      l_lobo.append(pp)
+
+  if q=='2':
+    print(l_lobo)
+    print(l_bech)
+    print(l_pork)
+# juice_list = ["1", "2"]
+# while True:
+#     print("1  想加入菜單的果汁")
+#     print("2  顯示菜單")
+#     print("3  結束程式")
+#     a = input("選擇步驟:")
+#     if a == "1":
+#         new = input("輸入果汁:")
+#         if new in juice_list:
+#           print("已有此果汁")
+#         else:
+#           juice_list.append(new)
+#           print("新增完成")
+#          for l in juice_list:
+#             if new == l:
+#                 print("已有此果汁")
+#                  break
+#          else:
+#              juice_list.append(new)
+#              print("新增完成")
+#     if a == "2":
+#         print(juice_list)
+#     if a == "3":
+#         print("結束程式")
+
